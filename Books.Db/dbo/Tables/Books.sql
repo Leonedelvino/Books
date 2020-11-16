23f35f42b7ebf7c8184fae64f89bf4e1171beb2a
Lev Kochin <rat.ratio@yandex.ru>
2020-11-16 09:58:46 +0600

    Rename project

--- Books.Db/dbo/Tables/Books.sql
+++ Books.Db/dbo/Tables/Books.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Books] (
     [Id]          INT             IDENTITY(1,1) NOT NULL,
     [Name]        NVARCHAR (200)  NOT NULL,
-    [Author]      NVARCHAR (150)  NULL,
+    [Author]      NVARCHAR (150)  NOT NULL,
+    [Description] NVARCHAR (1000) NULL,
     [Date]        DATE            NOT NULL,
     [Language]    NVARCHAR (100)  NOT NULL,
     [ImgUrl]      NVARCHAR (2048) NULL,
--- Books/Models/BookModel.cs
+++ Books/Models/BookModel.cs
@@ -17,6 +17,10 @@ namespace Books.Models
         [StringLength(150, ErrorMessage = "Слишком длинное имя автора книги, введите имя меньше 150 символов")]
         public string Author { get; set; }
 
+        [Required(ErrorMessage = "Укажите описание книги")]
+        [StringLength(1000, ErrorMessage = "Слишком длинное описание книги, введите описание меньше 1000 символов")]
+        public string Description { get; set; }
+
         public DateTime Date { get; set; }
 
         public string Language { get; set; }
