9fb54d9f9abf682016465256715971e969c17313
Lev Kochin <rat.ratio@yandex.ru>
2020-11-17 15:11:21 +0600

    Add material styles all package. Edit database and experemental edition for index

--- Books.Db/dbo/Tables/Books.sql
+++ Books.Db/dbo/Tables/Books.sql
@@ -2,7 +2,7 @@
     [Id]          INT             IDENTITY(1,1) NOT NULL,
     [Name]        NVARCHAR (200)  NOT NULL,
     [Author]      NVARCHAR (150)  NOT NULL,
-    [Description] NVARCHAR (1000) NULL,
+    [Description] NVARCHAR (MAX)  NULL,
     [Date]        DATE            NOT NULL,
     [Language]    NVARCHAR (100)  NOT NULL,
     [ImgUrl]      NVARCHAR (2048) NULL,
